09009a6b8385d5d3bae2d0c1d6283c1160f93506
Leonardo Cserny <leonardo_cserny@netex.ro>
2018-05-11 07:33:38 +0300

    adds loading screen

---
+++ core/src/net/cserny/game/LoadingScreen.java
@@ -0,0 +1,84 @@
+package net.cserny.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class LoadingScreen extends ScreenAdapter {
+
+    private static final float WORLD_WIDTH = 640;
+    private static final float WORLD_HEIGHT = 480;
+    private static final float PROGRESSBAR_WIDTH = 100;
+    private static final float PROGRESSBAR_HEIGHT = 25;
+
+    private final PeteGame game;
+
+    private ShapeRenderer shapeRenderer;
+    private Viewport viewport;
+    private Camera camera;
+    private float progress = 0;
+
+    public LoadingScreen(PeteGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void show() {
+        camera = new OrthographicCamera();
+        camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
+        camera.update();
+
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+
+        shapeRenderer = new ShapeRenderer();
+    }
+
+    @Override
+    public void render(float delta) {
+        update();
+        clearScreen();
+        draw();
+    }
+
+    private void update() {
+        if (game.getAssetManager().update()) {
+            game.setScreen(new GameScreen(game));
+        } else {
+            progress = game.getAssetManager().getProgress();
+        }
+    }
+
+    private void clearScreen() {
+        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    private void draw() {
+        shapeRenderer.setProjectionMatrix(camera.projection);
+        shapeRenderer.setTransformMatrix(camera.view);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.WHITE);
+        shapeRenderer.rect(
+                (WORLD_WIDTH - PROGRESSBAR_WIDTH) / 2,
+                WORLD_HEIGHT / 2 - PROGRESSBAR_HEIGHT / 2,
+                progress * PROGRESSBAR_WIDTH,
+                PROGRESSBAR_HEIGHT);
+        shapeRenderer.end();
+    }
+
+    @Override
+    public void dispose() {
+        shapeRenderer.dispose();
+    }
+}
--- core/src/net/cserny/game/PeteGame.java
+++ core/src/net/cserny/game/PeteGame.java
@@ -1,11 +1,18 @@
 package net.cserny.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 
 public class PeteGame extends Game {
 
+	private final AssetManager assetManager = new AssetManager();
+
 	@Override
 	public void create () {
-		setScreen(new GameScreen(this));
+		setScreen(new LoadingScreen(this));
+	}
+
+	public AssetManager getAssetManager() {
+		return assetManager;
 	}
 }
